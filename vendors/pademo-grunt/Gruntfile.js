da13199ee6d69264d3a3cb7cf2a25d68e8017d05
fgarcia <fgarcia@zenbits.io>
2014-08-16 21:10:35 +0200

    initial Grunt setup

---
+++ vendors/pademo-grunt/Gruntfile.js
@@ -0,0 +1,21 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    sass: {
+      dist: {
+        files: {
+          'public/css/style.css' : 'app/stylesheets/style.scss'
+        }
+      }
+    },
+    watch: {
+      css: {
+        files: 'app/stylesheets/**/*.scss',
+        tasks: ['sass']
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.registerTask('default',['watch']);
+}
--- vendors/pademo-grunt/README.md
+++ vendors/pademo-grunt/README.md
@@ -1,1 +1,11 @@
 Asset pipeline using Grunt
+
+NOTES:
+
+The best way to install Grunt requires setting up the folder also as a NodeJS project, which basically means:
+  - Define a package.json
+  - Commit downloaded modules into repo (node_modules)
+
+Once Grunt takes over our assets, we can no longer rely on Padrino auto
+reloading feature. Since this means that now we must run the Grunt watcher
+along the rack server, we use Foreman to ease starting the services. Have a look at the Procfile
